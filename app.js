0509ee5c0348da3f605e11ea5039f7807185456d
Marc Asbach <marc.asbach@web.de>
2017-05-04 10:23:54 +0200

    first commit;

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+    const express = require("express");
+    const server = express();
+
+    // make server write a no-cache header
+    function addNoCacheHeaders (request, response, next) {
+        response.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        response.header('Expires', '-1');
+        response.header('Pragma', 'no-cache');
+        next();
+    }
+
+    server.use(addNoCacheHeaders)
+    server.use(express.static(__dirname + "/app"));
+    server.listen(1337);
+})()
