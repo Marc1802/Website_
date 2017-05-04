48ad3dde67f44dc3c88fbb1e05da69ebb6cb6116
Basti Tee <basti.tee@posteo.de>
2017-05-04 14:00:06 +0200

    added upload script; added htaccess;

---
+++ upload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# wechsel in skriptverzeichnis
+cd "$( dirname "$( readlink -f "$0" )")"
+
+# teste ob ziel url gesetzt ist 
+if [ -z "$1" ]; then
+    echo "Kein username@servername angegeben."
+    exit 1
+fi
+
+# alte zip datei löschen
+rm -f app.zip
+# in app verzeichnis wechseln
+cd app/
+# inhalte im app verzeichnis zippen
+zip -r ../app.zip *
+# zurück nach hauptverzeichnis
+cd ..
+# auf webspace den inhalt von html/ löschen
+ssh ${1} "rm -rf ~/html/*"
+# zip datei auf webspace hochladen
+scp app.zip ${1}:~/html
+# zip datei auf webspace entpacken
+ssh ${1} "cd html && unzip app.zip"
